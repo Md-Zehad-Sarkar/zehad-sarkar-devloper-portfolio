c773ddf2fafbb2a697623c47c96b64543712a540
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2024-03-20 17:36:15 +0600

    project added

--- src/components/shared_pages/projects/Two.tsx
+++ src/components/shared_pages/projects/Two.tsx
@@ -1,32 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
-import image_1 from "@/assets/images/project-1.png";
+import image_2 from "@/assets/images/project-2.png";
 
-const One = () => {
+const Two = () => {
   return (
     <div className="card max-w-[800px] w-full  shadow-xl bg-gradient-to-r from-purple-500 to-fuchsia-500 rounded-md  hover:bg-gradient-to-l border-2 border-purple-500">
       <figure>
-        <Image src={image_1} alt="ECR" height={600} className="max-w-[800px] w-full" />
+        <Image
+          src={image_2}
+          alt="ECR"
+          height={600}
+          className="max-w-[800px] w-full"
+        />
       </figure>
       <div className="card-body">
         <h2 className="card-title text-white font-thin">
-          site name: Empowering Recovery chain
+          site name: Toy Thriver
         </h2>
         <p className="text-white">
-          Technologies: typescript, react, tailwind, redux
+          Technologies: react,, mongodb, express js, firebase, tailwind
         </p>
         <div className="card-actions">
           <div className="badge badge-outline text-white font-medium py-3">
-            <Link
-              href="https://empowering-recovery-chain-client-site.netlify.app"
-              target="_blank"
-            >
+            <Link href="https://toy-thriver.web.app/" target="_blank">
               UI live link
             </Link>
           </div>
           <div className="badge badge-outline text-white font-medium py-3">
             <Link
-              href="https://github.com/Md-Zehad-Sarkar/empowering-recovery-chain-server-final.git"
+              href="https://github.com/Zehad-Sarkar/toy-thriver-server-side"
               target="_blank"
             >
               Server git link
@@ -34,7 +36,7 @@ const One = () => {
           </div>
           <div className="badge badge-outline text-white font-medium py-3">
             <Link
-              href="https://github.com/Md-Zehad-Sarkar/empowering-recovery-chain-client-final.git"
+              href="https://github.com/Zehad-Sarkar/toy-thriver-client-side-repo.git"
               target="_blank"
             >
               Client git link
@@ -46,4 +48,4 @@ const One = () => {
   );
 };
 
-export default One;
+export default Two;
--- src/components/shared_pages/projects/four.tsx
+++ src/components/shared_pages/projects/four.tsx
@@ -1,32 +1,43 @@
 import Image from "next/image";
 import Link from "next/link";
-import image_1 from "@/assets/images/project-1.png";
+import image_4 from "@/assets/images/project-4.jpg";
+import image_4_2 from "@/assets/images/project-4.2.jpg";
 
-const One = () => {
+const Four = () => {
   return (
     <div className="card max-w-[800px] w-full  shadow-xl bg-gradient-to-r from-purple-500 to-fuchsia-500 rounded-md  hover:bg-gradient-to-l border-2 border-purple-500">
       <figure>
-        <Image src={image_1} alt="ECR" height={600} className="max-w-[800px] w-full" />
+        <Image
+          src={image_4}
+          alt="ECR"
+          height={600}
+          className="max-w-[800px] w-full"
+        />
+      </figure>
+      <figure>
+        <Image
+          src={image_4_2}
+          alt="ECR"
+          height={600}
+          className="max-w-[800px] w-full"
+        />
       </figure>
       <div className="card-body">
         <h2 className="card-title text-white font-thin">
-          site name: Empowering Recovery chain
+          site name: Tasty Cookes Compass
         </h2>
         <p className="text-white">
-          Technologies: typescript, react, tailwind, redux
+          Technologies: react, express js, firebase, tailwind and flowbite ui
         </p>
         <div className="card-actions">
           <div className="badge badge-outline text-white font-medium py-3">
-            <Link
-              href="https://empowering-recovery-chain-client-site.netlify.app"
-              target="_blank"
-            >
+            <Link href="https://tasty-cookes-compass.web.app/" target="_blank">
               UI live link
             </Link>
           </div>
           <div className="badge badge-outline text-white font-medium py-3">
             <Link
-              href="https://github.com/Md-Zehad-Sarkar/empowering-recovery-chain-server-final.git"
+              href="https://github.com/Zehad-Sarkar/tasty-cooks-compass-server.git"
               target="_blank"
             >
               Server git link
@@ -34,7 +45,7 @@ const One = () => {
           </div>
           <div className="badge badge-outline text-white font-medium py-3">
             <Link
-              href="https://github.com/Md-Zehad-Sarkar/empowering-recovery-chain-client-final.git"
+              href="https://github.com/Zehad-Sarkar/tasty-cooks-compass-client.git"
               target="_blank"
             >
               Client git link
@@ -46,4 +57,4 @@ const One = () => {
   );
 };
 
-export default One;
+export default Four;
