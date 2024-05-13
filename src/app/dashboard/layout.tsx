b4505e667f29905e017be0b2eda1aaf00877d458
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2024-05-13 12:36:16 +0600

    add-blog page delete from dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,5 @@
+"use client";
 import React from "react";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "dashboard",
-  description: `Dashboard Develop by 'Zehad Sarkar'`,
-};
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return <div className="max-w-[1200px] w-full mx-auto">{children}</div>;
