b6c6c194e977eab223a9ce1767a22d78e97290a6
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2024-05-23 09:04:20 +0600

    resume link and cv link updated

--- src/components/shared_pages/Resume.tsx
+++ src/components/shared_pages/Resume.tsx
@@ -1,12 +1,22 @@
 'use client'
 const Resume = () => {
   //resume download
-  const resumeLink = `https://drive.google.com/file/d/1c-B8cVtNF9Mrud8xiGaWfJez5wODfXVj/view?usp=sharing`;
+  const resumeLink = `https://drive.google.com/file/d/15dB2C1Qmcq7Xt9zZr3dA-qfFhyos8XrP/view?usp=drive_link`;
+
+  const cvLink = `https://drive.google.com/file/d/17z9PQy3PL_Nc3enKSSLk4MTrnajhhlOO/view?usp=drive_link`;
 
   const handleDownload = () => {
     const link = document.createElement("a");
     link.href = resumeLink;
-    link.download = "frontendDeveloper.pdf";
+    link.download = "Zehad_Sarkar_Resume.pdf";
+    link.click();
+  };
+
+  // cv download link event handler
+  const handleCvDownload = () => {
+    const link = document.createElement("a");
+    link.href = cvLink;
+    link.download = "Zehad_Sarkar_CV.pdf";
     link.click();
   };
 
@@ -20,6 +30,15 @@ const Resume = () => {
         <span
           className="btn btn-active btn-link text-xl"
           onClick={handleDownload}
+        >
+          Download
+        </span>
+      </div>
+      <div className="flex justify-start items-center">
+        <p className="text-xl"> See the CV click on </p>
+        <span
+          className="btn btn-active btn-link text-xl"
+          onClick={handleCvDownload}
         >
           Download
         </span>
